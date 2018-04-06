b5416cfbf69678ef97e009ffb04ed513f0110673
Yajana <31155354+Yajan@users.noreply.github.com>
2018-04-06 20:16:35 +0530

    Update Logger.py

--- Distributed-Setup/Logger.py
+++ Distributed-Setup/Logger.py
@@ -1,11 +1,14 @@
 import logging
-logger = logging.getLogger('__name__'
-                           '')
+from colorlog import ColoredFormatter,getLogger
+#logger = logging.getLogger('__name__')
+logger = getLogger()
 logger.setLevel(logging.DEBUG)
 
 # create console handler and set level to debug
 ch = logging.StreamHandler()
 ch.setLevel(logging.WARN)
+
+
 
 # create a file handler
 fh = logging.FileHandler('program.log')
@@ -14,9 +17,24 @@ fh.setLevel(logging.INFO)
 # create formatter
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s',datefmt='%m/%d/%Y %I:%M:%S %p')
 
+
 # add formatter to ch
 ch.setFormatter(formatter)
 fh.setFormatter(formatter)
+
+col_formatter = ColoredFormatter(
+        "%(cyan)s%(asctime)s %(log_color)s%(levelname)-8s%(reset)s %(blue)s%(message)s",
+        datefmt='%I:%M:%S %p',
+        reset=True,
+        log_colors={
+                'DEBUG':    'cyan',
+                'INFO':     'green',
+                'WARNING':  'yellow',
+                'ERROR':    'red',
+                'CRITICAL': 'red',
+        }
+)
+ch.setFormatter(col_formatter)
 
 # add ch to logger
 logger.addHandler(ch)
